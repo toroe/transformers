6c097e84090a02bb6186a8408bb6e5b937f9d326
Paul Grundmann <paul.grundmann@snafu.de>
2021-07-30 16:38:56 +0200

    Fix Fnet implementation

--- src/transformers/models/fnet/modeling_fnet.py
+++ src/transformers/models/fnet/modeling_fnet.py
@@ -59,7 +59,7 @@ from .configuration_fnet import FnetConfig
 
 logger = logging.get_logger(__name__)
 
-_CHECKPOINT_FOR_DOC = "bert-base-uncased"
+_CHECKPOINT_FOR_DOC = ""
 _CONFIG_FOR_DOC = "FnetConfig"
 _TOKENIZER_FOR_DOC = "BertTokenizer"
 
@@ -252,13 +252,13 @@ class FnetSelfAttention(nn.Module):
 class FnetSelfOutput(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        #self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        #self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, hidden_states, input_tensor):
-        hidden_states = self.dense(hidden_states)
-        hidden_states = self.dropout(hidden_states)
+        #hidden_states = self.dense(hidden_states)
+        #hidden_states = self.dropout(hidden_states)
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
         return hidden_states
 
